49fd68bea4d6554741bb617a562876ed73dcc877
robin <robenjay@hotmail.nl>
2024-03-07 14:52:34 +0100

    added a little styling, not sure how it works

---
+++ TamagotchiCSharpThreading/LoadScreenPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TamagotchiCSharpThreading
+{
+    public partial class LoadScreenPage : ContentPage
+    {
+        int count = 0;
+
+        public LoadScreenPage()
+        {
+            InitializeComponent();
+        }
+
+        /*private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }*/
+    }
+}
