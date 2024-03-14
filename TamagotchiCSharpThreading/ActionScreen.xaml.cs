6a071b0f1ad255a98b1c8732cd7b24a17a4c1b9e
robin <robenjay@hotmail.nl>
2024-03-14 12:59:34 +0100

    set up the styling for the page

--- TamagotchiCSharpThreading/ActionScreen.xaml.cs
+++ TamagotchiCSharpThreading/ActionScreen.xaml.cs
@@ -1,7 +1,30 @@
-﻿namespace TamagotchiCSharpThreading
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TamagotchiCSharpThreading
 {
     public partial class ActionScreen : ContentPage
     {
+        int count = 0;
 
+        public ActionScreen()
+        {
+            InitializeComponent();
+        }
+
+        /*private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }*/
     }
 }
